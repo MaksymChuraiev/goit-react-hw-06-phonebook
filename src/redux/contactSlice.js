f58dc4d83ade140a20af09db4bec6fc7033a0430
MaksymChuraiev <mchuraev1988@gmail.com>
2022-05-13 21:23:14 +0300

    Add contacts slice

--- src/redux/contactSlice.js
+++ src/redux/contactSlice.js
@@ -1,9 +1,35 @@
-// const { createSlice } = require("@reduxjs/toolkit");
+import { createSlice } from '@reduxjs/toolkit';
+import { persistReducer } from 'redux-persist';
+// const { createSlice } = require('@reduxjs/toolkit');
+import storage from 'redux-persist/lib/storage';
 
-// const contactsSlice = createSlice({
-//   name: 'contacts',
-//   initialState: {
-//     items: [],
-//     filter:''
-//   }
-//  })
+export const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: {
+    items: [],
+    filter: '',
+  },
+  reducers: {
+    createContacts(state, action) {
+      state.items.push(action.payload);
+    },
+    addFilter(state, action) {
+      state.filter = action.payload;
+    },
+  },
+});
+
+const persistConfig = {
+  key: 'contacts',
+  storage,
+};
+
+export const contactsReducer = persistReducer(
+  persistConfig,
+  contactsSlice.reducer
+);
+
+export const { createContacts, addFilter } = contactsSlice.actions;
+
+export const getContacts = ({ contacts }) => contacts.items;
+export const getFilter = ({ contacts }) => contacts.filter;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,27 @@
 import { configureStore } from '@reduxjs/toolkit';
+import {
+  persistStore,
+  FLUSH,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+  REHYDRATE,
+} from 'redux-persist';
+
+import { contactsSlice } from './contactSlice';
 
 export const store = configureStore({
   reducer: {
-    contacts: 'reducerContacts',
+    contacts: contactsSlice.reducer,
+  },
+  middleware(getDefaultMiddleware) {
+    return getDefaultMiddleware({
+      serializableCheck: {
+        ignoreActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    });
   },
 });
+
+export const persistor = persistStore(store);
